6453adbaf5cfd7a962870718cd8a64319d78c164
Andy Birosak <abirosak@gmail.com>
2019-10-01 16:29:00 -0400

    started greek quiz2

---
+++ museum-quiz/src/components/GreekQuiz2.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
+
+class GreekQuiz2 extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quiz: this.props.quiz.questions[1]
+        }
+    }
+    
+    getAnswer = (value) => {
+        console.log(value)
+        if (value) {
+            this.props.history.push('/greek-cor-ans2')
+        } else {
+            this.props.history.push('/greek-inc-ans2')
+        }
+    }
+
+    render () {
+        // this.getAnswer()
+        const quiz = this.props.quiz.questions[1] 
+        // console.log(quiz.a.isCorrect
+        return(
+            <div>
+                <h3>{quiz.title}</h3>
+                <p onClick={() => {this.getAnswer(quiz.a.isCorrect)}}> {quiz.a.option}</p>
+                <p onClick={() => {this.getAnswer(quiz.b.isCorrect)}}> {quiz.b.option}</p>
+                <p onClick={() => {this.getAnswer(quiz.c.isCorrect)}}> {quiz.c.option}</p>
+                <p onClick={() => {this.getAnswer(quiz.d.isCorrect)}}> {quiz.d.option}</p>
+            </div>
+        )
+    }
+}
+
+export default withRouter(GreekQuiz2)
