665421561739010df730a59494868a0320c2a018
Andy Birosak <abirosak@gmail.com>
2019-10-01 09:43:23 -0400

    greekQuiz questions complete

--- museum-quiz/src/greek-quiz.js
+++ museum-quiz/src/greek-quiz.js
@@ -1,52 +1,55 @@
-{
-    {
-        "A 'silen' was a mythical companion for which Greek god?": [
-          {
+const greekQuiz = {
+  questions: [
+    
+        {
+          "question1": "A 'silen' was a mythical companion for which Greek god?", 
+          
             "i1": "Aphrodite",
             "a": "Donysos",
             "i3": "Demeter",
             "i4": "Hera"
-          }
-        ]
-    }
+          
+        
+    },
     {
-        "What is a Nereid?": [
-          {
+        "question2": "What is a Nereid?",
+          
             "i1": "Aphrodite",
             "i2": "Donysos",
             "a": "The sea nymph daughters of Nereus",
             "i4": "Hera"
-          }
-        ]
-    }
+          
+        
+    },
     {
-        "The phylakes depicted in the mixing bowl could also be described as": [
-          {
+        "question3": "The phylakes depicted in the mixing bowl could also be described as", 
+          
             "i1": "Aphrodite",
             "i2": "Donysos",
             "i3": "Demeter",
             "a": "actors"
-          }
-        ]
-    }
+          
+        
+    },
     {
-        "What were strigils used for in ancient Greece?": [
-          {
+        "question4": "What were strigils used for in ancient Greece?", 
+          
             "a": "to scrape oil, sweat, and sand off the body after exercise",
             "i2": "Donysos",
             "i3": "Demeter",
             "i4": "Hera"
-          }
-        ]
-    }
+          
+        
+    },
     {
-        "Items made out of which material were valued for their golden sheen, versatility, and durability?": [
-          {
+        "question5": "Items made out of which material were valued for their golden sheen, versatility, and durability?", 
+          
             "i1": "Aphrodite",
             "a": "copper alloy",
             "i3": "Demeter",
             "i4": "Hera"
-          }
-        ]
+          
+        
     }
+  ]
 }
