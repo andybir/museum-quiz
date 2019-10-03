9c354f0c9a98976704e030f3ba1c86efb59d7825
Andy Birosak <abirosak@gmail.com>
2019-10-03 16:33:56 -0400

    korean quiz components built

---
+++ museum-quiz/src/components/KoreanQuiz4.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+
+class KoreanQuiz4 extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            quiz: this.props.quiz.questions[13]
+        }
+    }
+    
+    getAnswer = (value) => {
+        console.log(value)
+        if (value) {
+            this.props.history.push('/korean-correct4')
+        } else {
+            this.props.history.push('/korean-incorrect4')
+        }
+        // Modal???    
+        } 
+    
+    render () {
+        // this.getAnswer()
+        const quiz = this.props.quiz.questions[13] 
+        // console.log(quiz.a.isCorrect
+        return(
+            <div className='quiz'>
+                <h2>{quiz.title}</h2>
+                
+                <p className='quiz-choice' onClick={() => {this.getAnswer(quiz.a.isCorrect)}}> {quiz.a.option}</p>
+                <p className='quiz-choice' onClick={() => {this.getAnswer(quiz.b.isCorrect)}}> {quiz.b.option}</p>
+                <p className='quiz-choice' onClick={() => {this.getAnswer(quiz.c.isCorrect)}}> {quiz.c.option}</p>
+                <p className='quiz-choice' onClick={() => {this.getAnswer(quiz.d.isCorrect)}}> {quiz.d.option}</p>
+                <button><Link to={'/korean-art4'}>Go Back</Link></button>
+                
+            </div>
+        )
+    }
+}
+
+export default withRouter(KoreanQuiz4)
